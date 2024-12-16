c4ecf20b2735a8f7deaef098795839c303128e3d
Chipok27 <chipok27@Gmail.com>
2024-12-16 22:34:09 +0700

    Grade 1. Этап 1: Задание 3

--- date_changer.py
+++ date_changer.py
@@ -1,5 +1,25 @@
-import greetings
-temp_created_date = greetings.created_date
-temp_issue_date = greetings.issue_date
-print(temp_created_date)
-print(temp_issue_date)
+# Grade 1. Этап 1: Задание 2
+# ЗАМЕТКИ
+
+username = 'Пользователь #1'
+title = 'Первая заметка'
+content = 'Заметили первую заметку'
+# 0 - ожидает, 1 - в работе, 2 - готово
+status = 0
+created_date = '10-11-2024'
+issue_date = '10-12-2024'
+
+print('Имя пользователя:', username)
+print('Заголовок заметки:', title)
+print('Описание заметки:', content)
+if status == 0:
+    print('Статус заметки:', 'ожидает')
+if status == 1:
+    print('Статус заметки:', 'в работе')
+if status == 2:
+    print('Статус заметки:', 'готово')
+print('Дата создания заметки:', created_date[:-5])
+print('Дата актуальности заметки', issue_date[:-5])
+
+
+
